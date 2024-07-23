5e75acc7ef9059815ea4027478f35849e39375e4
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-23 08:43:22 +0100

    etst

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+    let task = false;
+    let task2 = true;
+  
+    if (!true) {
+      task = true;
+      task2 = false;
+    }
+  
+    return [task, task2];
+  }
