a4fba142be82d2981cd7033a7b22382845156222
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-23 08:29:19 +0100

    etst

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,15 @@
-export const  taskFirst = ()=> {
-    var task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export let taskNext = ()=> {
-    var combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
