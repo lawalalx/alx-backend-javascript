9211851307ab7b0c8745387b603b9dd09bd4e6cd
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-24 02:02:46 +0100

    test

---
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -0,0 +1,11 @@
+export default function getSanFranciscoDescription() {
+  const year = 2017;
+
+  const budget = {
+    income: '$119,868',
+    gdp: '$154.2 billion',
+    capita: '$178,479',
+  };
+
+  return `In ${year}, San Francisco had a total budget of ${budget.income} with a gross domestic product of ${budget.gdp} per capita income was ${budget.capita}.`;
+}
