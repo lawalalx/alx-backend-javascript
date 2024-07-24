9a7fdf31b0e3b085ddc8c4320692baca01fd7d8a
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-07-24 19:37:54 +0100

    etst

--- 0x01-ES6_promise/main.js
+++ 0x01-ES6_promise/main.js
@@ -1,12 +1,16 @@
-import signUpUser from './4-user-promise.js';
+// import signUpUser from './4-user-promise.js';
 
-export default function handleProfileSignup(firstName, lastName) {
-  return signUpUser(firstName, lastName)
-    .then((data) => {
-      console.log(data);
-    })
-    .catch(() => console.log('Signup system offline'));
-}
+// export default function handleProfileSignup(firstName, lastName) {
+//   return signUpUser(firstName, lastName)
+//     .then((data) => {
+//       console.log(data);
+//     })
+//     .catch(() => console.log('Signup system offline'));
+// }
 
 
-console.log(await signUpUser("Olanrewaju", "Oluwafemi"));
+// console.log( signUpUser("Olanrewaju", "Oluwafemi"));
+
+import add from "./ind.js"
+
+console.log(await add(1, 2));
